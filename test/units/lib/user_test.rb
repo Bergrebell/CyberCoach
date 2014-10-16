3f801208259d0088e4ba071c703c45788cc9e7e3
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-10-16 22:48:29 +0200

    refactor and add some comments in rest adapter

--- test/units/lib/user_test.rb
+++ test/units/lib/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 require 'pp'
 
-class TestRestAdapter  < ActiveSupport::TestCase
+class TestRestAdapter < ActiveSupport::TestCase
 
   # Test class methods
 
@@ -19,9 +19,14 @@ class TestRestAdapter  < ActiveSupport::TestCase
   end
 
 
-
   test "create user object" do
-    user = RestAdapter::User.new username: 'alex', email: 'alex@test.com', password: 'test', public_visible: RestAdapter::Privacy::Public
+    user = RestAdapter::User.new(
+        username: 'alex',
+        email: 'alex@test.com',
+        password: 'test',
+        public_visible: RestAdapter::Privacy::Public
+    )
+
     assert_equal '/CyberCoachServer/resources/users/alex', user.uri
     assert_equal 'http://diufvm31.unifr.ch:8090/CyberCoachServer/resources/users/alex', user.entity_uri
 
@@ -31,54 +36,61 @@ class TestRestAdapter  < ActiveSupport::TestCase
     assert_equal RestAdapter::Privacy::Public, user.public_visible
   end
 
+
   test "retrieve a user" do
-    user = RestAdapter::User.retrieve 'moritz'
+    user = RestAdapter::User.retrieve 'asarteam1'
     assert_not_nil user
-    assert_equal 'moritz', user.username
+    assert_equal 'asarteam1', user.username
     assert_equal RestAdapter::Privacy::Public, user.public_visible
-    assert_equal 'Moritz Cheng', user.real_name
-    assert_equal '/CyberCoachServer/resources/users/moritz/', user.uri
+    assert_equal 'asarteam1', user.real_name
+    assert_equal '/CyberCoachServer/resources/users/asarteam1/', user.uri
   end
 
+
   test "retrieve a collection of five users" do
-    users = RestAdapter::User.all query: {start: 0, size: 5}
+    users = RestAdapter::User.all(query: {start: 0, size: 5})
     assert_not_nil users
     assert_equal 5, users.size
   end
+
 
   test "retrieve a all users" do
     users = RestAdapter::User.all
     assert_not_nil users
   end
 
+
   test "filter users" do
-    friends = ['alex','moritz','timon']
-    users = RestAdapter::User.all filter: ->(user) { friends.include?(user.username)}
-    assert_equal 3, users.size
+    friends = ['asarteam1','asarteam2','asarteam3','asarteam4','asarteam5']
+    users = RestAdapter::User.all filter: ->(user) { friends.include?(user.username) }
+    assert_equal 5, users.size
   end
 
+
   test "fetch details of a user" do
-    users = RestAdapter::User.all filter: ->(user) { user.username == 'moritz' }
+    users = RestAdapter::User.all filter: ->(user) { user.username == 'asarteam1' }
     user = users.first
     user.fetch!
-    assert_equal 'Moritz Cheng', user.real_name
+    assert_equal 'asarteam1', user.real_name
     assert_equal RestAdapter::Privacy::Public, user.public_visible
   end
 
+
   test "fetch details of a user in functional way" do
-    users = RestAdapter::User.all filter: ->(user) { user.username == 'moritz' }
+    users = RestAdapter::User.all filter: ->(user) { user.username == 'asarteam1' }
     user = users.first
     user = user.fetch
-    assert_equal 'Moritz Cheng', user.real_name
-       assert_equal RestAdapter::Privacy::Public, user.public_visible
+    assert_equal 'asarteam1', user.real_name
+    assert_equal RestAdapter::Privacy::Public, user.public_visible
   end
+
 
   test "authenticate user" do
-    user = RestAdapter::User.authenticate username: 'alex', password: 'scareface'
-
+    user = RestAdapter::User.authenticate username: 'asarteam1', password: 'scareface'
     assert user != false
-    assert_equal 'alex', user.username
+    assert_equal 'asarteam1', user.username
   end
+
 
   test "if user name is available" do
 
@@ -87,7 +99,7 @@ class TestRestAdapter  < ActiveSupport::TestCase
     assert check==false
 
     # a name that is already taken, should fail
-    check = RestAdapter::User.username_available?('moritz')
+    check = RestAdapter::User.username_available?('asarteam1')
     assert check==false
 
     # invalid name should fail
@@ -101,30 +113,32 @@ class TestRestAdapter  < ActiveSupport::TestCase
 
 
   test "update user" do
-    user = RestAdapter::User.retrieve 'moritz'
-    user.email = 'moritz.cheng@unifr.ch'
+    user = RestAdapter::User.retrieve 'asarteam1'
+    user.email = 'asarteam1@test.com'
 
-    auth_proxy = RestAdapter::AuthProxy.new username: 'moritz', password: 'scareface'
+    auth_proxy = RestAdapter::AuthProxy.new username: 'asarteam1', password: 'scareface'
     auth_proxy.save(user)
 
-    test_user = RestAdapter::User.retrieve 'moritz'
-    assert_equal 'moritz.cheng@unifr.ch', test_user.email
-
+    test_user = RestAdapter::User.retrieve 'asarteam1'
+    assert_equal 'asarteam1@test.com', test_user.email
   end
 
 
   test "create and delete user" do
-    auth_proxy = RestAdapter::AuthProxy.new username: 'dummy', password: 'dummy'
-    user = RestAdapter::User.new(username: 'dummy',
-                                 password: 'dummy',
-                                 real_name: 'dummy user',
-                                 email: 'dummy@dummy.ch',
-                                 public_visible: RestAdapter::Privacy::Public
+    user = RestAdapter::User.new(
+        username: 'dummy',
+        email: 'dummy@test.com',
+        password: 'dummy',
+        real_name: 'dummy',
+        real_name: 'dummy',
+        public_visible: RestAdapter::Privacy::Public
     )
 
-    assert user.save
-    sleep 2
+    auth_proxy = RestAdapter::AuthProxy.new username: 'dummy', password: 'dummy'
 
+    assert auth_proxy.save(user)
+
+    sleep 2
     user = RestAdapter::User.retrieve 'dummy'
     assert user!= false
 
@@ -133,7 +147,24 @@ class TestRestAdapter  < ActiveSupport::TestCase
 
     user = RestAdapter::User.retrieve 'dummy'
     assert user== false
+  end
 
+
+  test "create some users" do
+    if false
+      usernames = (1..5).to_a.map { |a| 'asarteam' + a.to_s}
+      usernames.each do |username|
+        email = "#{username}@test.com"
+        user = RestAdapter::User.new(
+            username: username,
+            email: email,
+            password: 'scareface',
+            public_visible: RestAdapter::Privacy::Public,
+            real_name: username
+        )
+        assert user.save
+      end
+    end
   end
 
 
