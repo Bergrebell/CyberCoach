3091777cc0e9a492e9eee08abc960870ace25e56
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-10-30 12:39:27 +0100

    add lib/fascade.rb

---
+++ lib/fascade.rb
@@ -0,0 +1,8 @@
+module Fascade
+
+  RestAdapter::User = Fascade::User
+  RestAdapter::User = Fascade::Partnership
+  RestAdapter::SportSession = Fascade::SportSession
+  RestAdapter::Sport = RestAdapter::Models::Sport
+
+end
