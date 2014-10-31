3fc3ff04d8b40038838a815c8e7a0142988fcb86
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-10-31 19:27:52 +0100

    refactor method edit in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
     page = request.query_parameters[:page].nil? ? 1 : request.query_parameters[:page]
     page = page.to_i
     size = 5
-    @users = Facade::User.all query: { size: size, start: (page-1)*size }
+    @users = Facade::User.all query: {size: size, start: (page-1)*size}
     pages = (Facade::User.available / size.to_f).ceil
-    @links =  (1..pages)
+    @links = (1..pages)
   end
 
   # GET /users/1
@@ -47,26 +47,25 @@ class UsersController < ApplicationController
   def create
     # create a cyber coach user
     @user = Facade::User.create user_params
-
-      if @user.save # if validation is ok, try to create the user
-        session[:username] = @user.username
-        session[:password] = @user.password
-        redirect_to welcome_index_path, notice: 'User was successfully created.'
-      else
-        flash[:notice] = 'Could not register. Cyber coach server is bitchy today!'
-        render :new
-      end
+    if @user.save # if validation is ok, try to create the user
+      session[:username] = @user.username
+      session[:password] = @user.password
+      redirect_to welcome_index_path, notice: 'User was successfully created.'
+    else
+      flash[:notice] = 'Could not register. Cyber coach server is bitchy today!'
+      render :new
+    end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-      if current_user.update(user_params)
-        redirect_to welcome_index_path, notice: 'User was successfully updated.'
-      else
-        @user = current_user
-        render :edit
-      end
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to welcome_index_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   # DELETE /users/1
@@ -80,13 +79,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = Facade::User.retrieve(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = Facade::User.retrieve(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username,:password,:password_confirmation,:email,:real_name)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation, :email, :real_name)
+  end
 end
