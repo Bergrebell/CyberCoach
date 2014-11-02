24e662bb00ca59421441f7ae1a0ba548468fcb2a
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-11-02 02:18:26 +0100

    add refactored and forgotten auth proxy

--- test/units/lib/rest_adapter/auth_proxy_test.rb
+++ test/units/lib/rest_adapter/auth_proxy_test.rb
@@ -5,14 +5,14 @@ class TestAuthProxy  < ActiveSupport::TestCase
     user = RestAdapter::Models::User.retrieve 'alex'
     user.email = 'alex.r@unifr.ch'
 
-    auth_proxy = RestAdapter::AuthProxy.new username: 'alex', password: 'scareface'
+    auth_proxy = RestAdapter::Proxy::Auth.new username: 'alex', password: 'scareface'
 
     assert auth_proxy.save(user)
 
   end
 
   test 'if auth proxy is valid' do
-    auth_proxy = RestAdapter::AuthProxy.new username: 'alex', password: 'scareface'
+    auth_proxy = RestAdapter::Proxy::Auth.new username: 'alex', password: 'scareface'
     assert auth_proxy.authorized?
   end
 
