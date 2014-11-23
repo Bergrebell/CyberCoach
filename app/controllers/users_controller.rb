Bergrebell/CyberCoach

commit 9af2e66754296987c7a1b72fc2f47221824d0f44

- Implement Friendship model, closes #72 - Make the invitations work for Running when creating or editing an entry
Sorry for this huge commit, we can delete the friendship controller again later I guess... this all can be handled easy now over the FriendsController already existing
Proposals are missing, we simply list all users from our app not yet befriended with the current_user

app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = Facade::User.retrieve(params[:id])
+    @user = Facade::User.find_by id: params[:id]
   end
 
   # GET /users/new

app/assets/javascripts/friendships.js.coffee
@@ -1,0 +1,3 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/

app/assets/stylesheets/friendships.css.scss
@@ -1,0 +1,3 @@
+// Place all the styles related to the Friendships controller here.
+// They will automatically be included in application.css.
+// You can use Sass (SCSS) here: http://sass-lang.com/

app/controllers/friends_controller.rb
@@ -5,10 +5,10 @@ class FriendsController < ApplicationController
   def index
     @friends = current_user.friends
     @requests_received = current_user.received_friend_requests
-    users = Facade::User.query do
-      User.all
-    end
-    @proposals = current_user.friend_proposals(users)
+    # users = Facade::User.query do
+    #   User.all
+    # end
+    @proposals = []
   end
 
 

app/controllers/friendships_controller.rb
@@ -1,0 +1,74 @@
+class FriendshipsController < ApplicationController
+  before_action :set_friendship, only: [:show, :edit, :update, :destroy]
+
+  # GET /friendships
+  # GET /friendships.json
+  def index
+    @friendships = Friendship.all
+  end
+
+  # GET /friendships/1
+  # GET /friendships/1.json
+  def show
+  end
+
+  # GET /friendships/new
+  def new
+    @friendship = Friendship.new
+  end
+
+  # GET /friendships/1/edit
+  def edit
+  end
+
+  # POST /friendships
+  # POST /friendships.json
+  def create
+    @friendship = Friendship.new(friendship_params)
+
+    respond_to do |format|
+      if @friendship.save
+        format.html { redirect_to @friendship, notice: 'Friendship was successfully created.' }
+        format.json { render :show, status: :created, location: @friendship }
+      else
+        format.html { render :new }
+        format.json { render json: @friendship.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /friendships/1
+  # PATCH/PUT /friendships/1.json
+  def update
+    respond_to do |format|
+      if @friendship.update(friendship_params)
+        format.html { redirect_to @friendship, notice: 'Friendship was successfully updated.' }
+        format.json { render :show, status: :ok, location: @friendship }
+      else
+        format.html { render :edit }
+        format.json { render json: @friendship.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /friendships/1
+  # DELETE /friendships/1.json
+  def destroy
+    @friendship.destroy
+    respond_to do |format|
+      format.html { redirect_to friendships_url, notice: 'Friendship was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friendship
+      @friendship = Friendship.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def friendship_params
+      params.require(:friendship).permit(:user_id, :friend_id, :confirmed)
+    end
+end

app/controllers/runnings_controller.rb
@@ -12,24 +12,13 @@ class RunningsController < ApplicationController
 
   def new
     @running = Facade::SportSession::Running.create(user: current_user)
-    @friends = current_user.friends # TODO This should return users of type Facade?
+    @friends = current_user.friends
   end
 
 
   def edit
     @running = Facade::SportSession::Running.find_by id: params[:id]
-
-    if Date.parse(@running.entry_date) < Date.today
-      # Can only edit results
-      @results = @running.sport_session_participants.where(:user_id => current_user.id).first!.result
-      render :edit_results
-    else
-      # Normal edit
-      @friends = current_user.friends
-      render :edit
-    end
-
-
+    @friends = current_user.friends
   end
 
   def show
@@ -41,19 +30,8 @@ class RunningsController < ApplicationController
   def create
     date_time_object = DateTime.strptime(sport_session_params[:entry_date], '%Y-%m-%d')
     entry_params = sport_session_params.merge({user: current_user, entry_date: date_time_object})
-    #entry_params[:users_invited] = User.select('id').where('username IN (?)', entry_params[:users_invited])
 
-    users_invited = []
-    if entry_params[:users_invited].present?
-      entry_params[:users_invited].each do |username|
-        user = User.where(:username => username).first
-        if user.present?
-          users_invited.push(user.id)
-        end
-      end
-    end
-    entry_params[:users_invited] = users_invited
-    @entry = Facade::SportSession.create(entry_params)
+    @entry = Facade::SportSession::Running.create(entry_params)
 
     if @entry.save
       redirect_to runnings_url, notice: 'Running session successfully created'
@@ -65,30 +43,14 @@ class RunningsController < ApplicationController
 
 
   def update
-    @running = Facade::SportSession.find_by id: params[:id]
+    @running = Facade::SportSession::Running.find_by id: params[:id]
+    date_time_object = DateTime.strptime(sport_session_params[:entry_date], '%Y-%m-%d')
+    entry_params = sport_session_params.merge({user: current_user, entry_date: date_time_object})
 
-    if Date.parse(@running.entry_date) < Date.today
-
-      # Save result
-      @results = @running.sport_session_participants.where(:user_id => current_user.id).first!.result
-      @results.length = results_params[:length]
-      @results.time = results_params[:time]
-
-      @results.save
-
-      # TODO Check for achievements
-
-      redirect_to runnings_url, notice: 'Saved details'
-
+    if @running.update(entry_params)
+      redirect_to runnings_url, notice: 'Running session successfully updated'
     else
-
-      # Update entry
-      entry_params = sport_session_params.merge({user: current_user})
-      if @running.update(entry_params)
-        redirect_to runnings_url, notice: 'Running session successfully updated'
-      else
-        render :edit
-      end
+      render :edit
     end
   end
 

app/helpers/friendships_helper.rb
@@ -1,0 +1,2 @@
+module FriendshipsHelper
+end

app/models/friendship.rb
@@ -1,0 +1,2 @@
+class Friendship < ActiveRecord::Base
+end

commit 26eeb7f3b0af52cc8d78663978cf433f071f0779

fix bug: /users/show/{username} introduced in commit: https://github.com/Bergrebell/CyberCoach/commit/9af2e66754296987c7a1b72fc2f47221824d0f44#diff-4e05ad0d64e6100656b63ad1e78f32c5R15

app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = Facade::User.find_by id: params[:id]
+    @user = Facade::User.find_by username: params[:id]
   end
 
   # GET /users/new