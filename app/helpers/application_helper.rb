225c5edf282581386b919c9299f0b1286316881a
Roman Kuepper <rrkuepper@gmail.com>
2014-11-24 15:06:56 +0100

    User Page Gravatar plus UIKit - multiple friends

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 module ApplicationHelper
-  def avatar_url(user)
-    if user.avatar_url.present?
-      user.avatar_url
-    else
-      default_url = "#{root_url}images/guest.png"
-      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-    end
+  def avatar_url(mail)
+      gravatar_id = Digest::MD5.hexdigest(mail)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d"
   end
 end
