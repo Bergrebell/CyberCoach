04cf2e11a5f2f1eda6597fee679cc50b5cad2afe
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-12-04 22:00:35 +0100

    simplify query to retrieve all user achievements in user_achievements controller

--- app/controllers/user_achievements_controller.rb
+++ app/controllers/user_achievements_controller.rb
@@ -1,10 +1,8 @@
 class UserAchievementsController < ApplicationController
 
   def index
-
     @achievements = Achievement.all
-    @user_achievements = UserAchievement.where('user_id' => current_user.id).map { |user_achievement| user_achievement.achievement}
-
+    @user_achievements = current_user.achievements
   end
 
 end
