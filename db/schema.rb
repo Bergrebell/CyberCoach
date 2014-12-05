a96eccbc89ef9ee719fd6f5aca0c4642f1a10006
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2014-12-05 11:34:23 +0100

    Add icon attribute to achievement model, close #93

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203072249) do
+ActiveRecord::Schema.define(version: 20141205113249) do
 
   create_table "achievements", force: true do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141203072249) do
     t.text     "rules"
     t.string   "sport"
     t.string   "description"
+    t.string   "icon"
   end
 
   create_table "boxing_participant_results", force: true do |t|
