66dc525da6d077e179990bce5cc5557598b67ba1
Roman Kuepper <rrkuepper@gmail.com>
2014-12-09 17:59:22 +0100

    preparing welcome controller to add friends to timeline

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,8 @@ class WelcomeController < ApplicationController
     @achievements = @user.latest_achievements
     @upcoming_sport_sessions = @user.upcoming_sport_sessions
     @past_sport_sessions = @user.past_sport_sessions
+    @requests_received = current_user.received_friend_requests
+    @friends = current_user.friends
     end
   end
 end
