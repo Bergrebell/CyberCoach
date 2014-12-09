75177524280c8a64b49de5b4f847217512f36249
Roman Kuepper <rrkuepper@gmail.com>
2014-12-09 17:26:05 +0100

    first version user timeline

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,12 @@ class WelcomeController < ApplicationController
   skip_before_action :require_login
 
   def index
+    if
+    @user = current_user
+    @friends = @user.friends
+    @achievements = @user.latest_achievements
+    @upcoming_sport_sessions = @user.upcoming_sport_sessions
+    @past_sport_sessions = @user.past_sport_sessions
+    end
   end
 end
