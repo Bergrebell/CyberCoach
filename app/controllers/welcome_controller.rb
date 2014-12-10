53a3879a12eafc95dd0d4d762acdefbc4eb20be7
Roman Kuepper <rrkuepper@gmail.com>
2014-12-10 18:38:01 +0100

    user timeline v2 - sport sessions added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,11 +4,9 @@ class WelcomeController < ApplicationController
   def index
     if
     @user = current_user
-    @friends = Friendship.where("user_id = 1 OR friend_id = 1")
     @achievements = @user.latest_achievements
-    @upcoming_sport_sessions = @user.upcoming_sport_sessions
-    @past_sport_sessions = @user.past_sport_sessions
-    @requests_received = current_user.received_friend_requests
+    @sports = @user.sport_sessions
+    #@requests_received = current_user.received_friend_requests
     @friends = current_user.friends
     end
   end
