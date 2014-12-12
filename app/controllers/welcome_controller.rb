0d4cd4fa721a9db1a13e2b66f563768aeb144651
Roman Kuepper <rrkuepper@gmail.com>
2014-12-12 20:29:53 +0100

    timline achievements added and icons added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   def index
     if
     @user = current_user
-    @achievements = @user.latest_achievements
+    @achievements = @user.achievements
     @sports = @user.sport_sessions
     #@requests_received = current_user.received_friend_requests
     @friends = current_user.friends
