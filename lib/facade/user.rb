f89b61e3013417a12f5d44c4b360342f67e5bd0f
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-12-13 13:17:01 +0100

    create missing rails user for existing coach user automatically on login

--- lib/facade/user.rb
+++ lib/facade/user.rb
@@ -18,10 +18,29 @@ module Facade
     def self.authenticate(username, password)
       coach_user = Coach.authenticate(username, password)
       if coach_user
-        ::User.find_by username: username
+        get_or_create_rails_user(coach_user, username, password)
       else
         false
       end
+    end
+
+
+    def self.get_or_create_rails_user(coach_user, username, password )
+      rails_user = ::User.find_by username: username
+      if rails_user.nil? # create a rails user
+        user = ::User.new username: username, password: password
+        user.save(validate: false, run_callbacks: false)
+
+        # subscribe user to all sports
+        proxy = Coach4rb::Proxy::Access.new username, password, Coach
+        [:running, :soccer, :boxing, :cycling].each do |sport|
+          Thread.new do
+            proxy.subscribe(coach_user, sport)
+          end
+        end
+        rails_user = ::User.find_by username: username
+      end
+      rails_user
     end
 
 
