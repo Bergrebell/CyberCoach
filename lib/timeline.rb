5dc9e35e552a7f15c26e42a26c888c6d6d4c0df5
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-12-14 11:45:42 +0100

    test

--- lib/timeline.rb
+++ lib/timeline.rb
@@ -80,7 +80,7 @@ class Timeline
 
   def self.items(a_user)
     id = a_user.id
-    achievements = a_user.achievements.map {|a| AchievementItem.new(a)}
+    achievements = a_user.achievements.order('user_achievements.created_at DESC').map {|a| AchievementItem.new(a)}
     friends = a_user.friends.map {|a| FriendItem.new(a)}
     sport_sessions = a_user.sport_sessions.map {|a| SportSessionItem.new(a)}
     items = achievements + friends + sport_sessions
