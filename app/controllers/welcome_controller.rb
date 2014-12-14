334878d72ab6518c2f90072506e695ef9ccb7ec8
Roman Kuepper <rrkuepper@gmail.com>
2014-12-14 12:58:55 +0100

    sportsmate logo added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
     if current_user
       @user = current_user
       @items = Timeline.items(current_user)
+      @friends = @user.friends
     end
   end
 end
